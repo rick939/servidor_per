2f2e86117ff1f8e0852fbfcc2e7a88bc6ae29bf7
rick939 <ricardomatheus924@gmail.com>
2022-01-14 17:15:20 -0300

    Adicionando a rota de Post do nome no servidor

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const express = require('express')
+const { v4: uuidv4 } = require('uuid')
+
 const app = express()
+app.use(express.json())
+
+const alunos = {}
 
 app.get('/', (req, res) => {
     res.json({msg: "Hello from Express!"})
+})
+
+app.post('/alunos', (req, res) => {
+    const aluno = req.body
+    const idAluno = uuidv4()
+    // console.log(aluno)
+    aluno.id = idAluno
+    alunos[idAluno] = aluno
+    res.json({msg: `Aluno chamado ${aluno.nome} foi adicionado com sucesso!`})
+})
+
+app.get('/alunos', (req, res) => {
+    res.json({alunos: Object.values(alunos)})
 })
 
 app.listen(8080, () => {
