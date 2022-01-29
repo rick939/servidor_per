57d721bf86ef68a84f6d5598741024cc8f2e5363
rick939 <ricardomatheus924@gmail.com>
2022-01-29 00:29:38 -0300

    Instalando as bibliotecas ajv e ajv-formats

--- index.js
+++ index.js
@@ -1,50 +1,20 @@
 const express = require('express')
-const { v4: uuidv4 } = require('uuid')
+// const { v4: uuidv4 } = require('uuid')
+const rotaMembro = require('./rotas/membro.rota')
 
 const app = express()
 app.use(express.json())
 
-const alunos = {}
+app.use('/membros', rotaMembro)
 
 app.get('/', (req, res) => {
     res.json({msg: "Hello from Express!"})
 })
 
-app.post('/alunos', (req, res) => {
-    const aluno = req.body
-    const idAluno = uuidv4()
-    // console.log(aluno)
-    aluno.id = idAluno
-    alunos[idAluno] = aluno
-    res.json({msg: `Aluno chamado: ${aluno.nome} foi adicionado com sucesso!`})
-})
-
-app.put('/alunos', (req, res) => {
-    const id = req.query.id
-    if (id && alunos[id]){
-        const aluno = req.body
-        aluno.id = id
-        alunos[id] = aluno
-        res.json({msg: `Aluno chamado: ${aluno.nome} foi atualizado com sucesso!`})
-    }else{
-        res.status(400).json({msg: "Aluno não encontrado!"})
-    }
-})
-
-app.delete('/alunos', (req, res) => {
-    const id = req.query.id
-    if (id && alunos[id]){
-        delete alunos[id]
-        res.json({msg: "Aluno foi deletado com sucesso!"})
-    }else{
-        res.status(400).json({msg: "Aluno não encontrado!"})
-    }
-})
-
-app.get('/alunos', (req, res) => {
-    res.json({alunos: Object.values(alunos)})
-})
-
 app.listen(8080, () => {
     console.log('Servidor pronto na porta 8080')
 })
+
+/*
+https://www.youtube.com/watch?v=a0CKxlL40DE&list=TLPQMjUwMTIwMjIZyJ_vMvnEdQ&index=2
+*/
