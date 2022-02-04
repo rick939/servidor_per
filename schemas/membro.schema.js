23867730c0542760a882289343ca83a98edded18
rick939 <ricardomatheus924@gmail.com>
2022-02-04 17:16:02 -0300

    Atualizando o projeto!

--- schemas/membro.schema.js
+++ schemas/membro.schema.js
@@ -2,8 +2,9 @@ module.exports = {
     type: "object",
     properties: {
         nome: {type: "string"},
-        magia: {type: "string"}
+        magia: {type: "string"},
+        esquadrao: {type: "string"}
     },
-    required: ["nome", "magia"],
+    required: ["nome", "magia", "esquadrao"],
     additionalProperties: false
 }
